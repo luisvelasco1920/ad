8dbc7cd5631c966847882ff3a6427230b6e6fb55
luisvelasco1920 <luis.velasco@iesserpis.org>
2019-09-25 12:12:45 +0200

    CMySql iniciado

---
+++ CMySql/CMySql/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Data;
+using MySql.Data.MySqlClient;
+
+namespace CMySql
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Acceso a dbprueba");
+
+            IDbConnection dbConnection = new MySqlConnection(
+                 "server=localhost;database=dbprueba;user=root;password=sistemas;ssl-mode=none"
+            );
+
+            dbConnection.Open();
+
+            IDbCommand dbCommand = dbConnection.CreateCommand();
+            dbCommand.CommandText = "select * from categoria";
+            IDataReader dataReader = dbCommand.ExecuteReader()
+
+
+
+            dbConnection.Close();
+        }
+    }
+}
