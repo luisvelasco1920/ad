fb31d89e9375b6dd7caca2517e2abe8167a7b1a3
luisvelasco1920 <luis.velasco@iesserpis.org>
2019-09-30 10:46:51 +0200

    dbprueba.sql con create table articulo

---
+++ dbprueba.sql
@@ -0,0 +1,17 @@
+create table categoria (
+  id serial primary key,
+  nombre varchar(50) not null unique
+);
+
+insert into categoria (nombre) values ('categoría 1');
+insert into categoria (nombre) values ('categoría 2');
+insert into categoria (nombre) values ('categoría 3');
+
+create table articulo (
+  id serial primary key,
+  nombre varchar(50) not null unique,
+  precio decimal(10,2),
+  categoria bigint unsigned,
+  foreign key categoria references categoria(id)
+);
+
