0cbbfef4e86255f58707c3165f471e4fd314831b
luisvelasco1920 <luis.velasco@iesserpis.org>
2020-01-03 09:27:43 +0100

    dbprueba.sql añadidas tablas cliente, pedido y pedidolinea

--- dbprueba.sql
+++ dbprueba.sql
@@ -15,3 +15,35 @@ create table articulo (
   foreign key (categoria) references categoria (id)
 );
 
+insert into articulo (nombre, precio, categoria) values ('artículo 1', 1.0, 1);
+insert into articulo (nombre, precio, categoria) values ('artículo 2', 2.0, 2);
+insert into articulo (nombre, precio, categoria) values ('artículo 3', 3.0, 3);
+
+create table cliente (
+  id serial primary key,
+  nombre varchar(50) not null unique
+);
+
+insert into cliente (nombre) values ('cliente 1');
+insert into cliente (nombre) values ('cliente 2');
+insert into cliente (nombre) values ('cliente 3');
+
+create table pedido (
+  id serial primary key,
+  fecha datetime not null unique,
+  cliente bigint unsigned not null,
+  importe numeric(10,2) not null,
+  foreign key (cliente) references cliente (id)
+);
+
+create table pedidolinea (
+  id serial primary key,
+  pedido bigint unsigned not null,
+  articulo bigint unsigned not null,
+  precio numeric(10,2) not null,
+  unidades numeric(10,2) not null,
+  importe numeric(10,2) not null,
+  foreign key (pedido) references pedido (id) on delete cascade,
+  foreign key (articulo) references articulo (id)
+);
+
